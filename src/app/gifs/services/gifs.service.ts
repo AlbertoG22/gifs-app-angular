ec09116e940b2685db43b361b6fac647bdef6eb1
AlbertoG22 <albertogarciamartinez99@gmail.com>
2024-04-18 17:23:09 -0600

    making http request with HttpClientModule

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,10 +1,13 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({providedIn: 'root'})
 export class GifsService {
   private _tagsHistory: string[] = [];
+  private apiKey: string = '';
+  private serviceUrl: string = 'https://api.giphy.com/v1/gifs';
 
-  constructor() { }
+  constructor( private http: HttpClient ) { }
 
   get tagsHistory() {
     return [ ...this._tagsHistory ];
@@ -30,6 +33,16 @@ export class GifsService {
     if( tag.length === 0 ) return;
 
     this.organizeHistory(tag);
+
+    const params = new HttpParams()
+      .set('api_key', this.apiKey)
+      .set('limit', '10')
+      .set('q', tag)
+
+    this.http.get(`${this.serviceUrl}/search`, { params })
+      .subscribe( resp => {
+        console.log(resp);
+      });
   }
 
 }
