b49efdccc41fe28fc811bb0e25c0ea8092015b22
AlbertoG22 <albertogarciamartinez99@gmail.com>
2024-04-19 13:35:03 -0600

    get history information using localStorage

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -11,7 +11,10 @@ export class GifsService {
   private apiKey: string = '';
   private serviceUrl: string = 'https://api.giphy.com/v1/gifs';
 
-  constructor( private http: HttpClient ) { }
+  constructor( private http: HttpClient ) {
+    this.loadLocalStorage();
+    console.log('Gifs service ready');
+  }
 
   get tagsHistory() {
     return [ ...this._tagsHistory ];
@@ -39,6 +42,15 @@ export class GifsService {
     localStorage.setItem('history', JSON.stringify(this._tagsHistory));
   }
 
+  private loadLocalStorage(): void {
+    if(!localStorage.getItem('history')) return;
+
+    this._tagsHistory = JSON.parse( localStorage.getItem('history')! );
+
+    if( this._tagsHistory.length === 0 ) return;
+    this.searchTag(this._tagsHistory[0]);
+  }
+
   searchTag( tag: string ): void {
     if( tag.length === 0 ) return;
 
@@ -52,7 +64,7 @@ export class GifsService {
     this.http.get<SearchResponse>(`${this.serviceUrl}/search`, { params })
       .subscribe( resp => {
         this.gifList = resp.data;
-        console.log({ gifs: this.gifList });
+        // console.log({ gifs: this.gifList });
       });
   }
 
